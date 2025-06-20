7948bd0115b0a203cb780e546efa7013e84949fd
Tang Piseth <tangpiseth43@gmail.com>
2025-06-20 11:07:42 +0000

    [automatic commit] 2025-06-20 11:07:42 UTC 129.127.202.120

--- part1/app.js
+++ part1/app.js
@@ -104,7 +104,9 @@ let db;
       `);
       console.log('Walk Requests inserted.');
 
-      // Insert WalkRatings (this wasn't specified in the previous exercise so if the following queries weren't executed, one would receive null or 0 data when browsed to the /api/walker/summary endpoint)
+      // Insert WalkRatings (this wasn't specified in the previous exercise so
+      // if the following queries weren't executed,
+      // one would receive null or 0 data when browsed to the /api/walker/summary endpoint)
       await db.execute(`
       INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES
       -- Bob's ratings
