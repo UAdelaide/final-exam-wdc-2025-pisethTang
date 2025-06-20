549ff788e0eb57c5c11ee8f50870d3f4aa94fc83
Tang Piseth <tangpiseth43@gmail.com>
2025-06-20 10:00:29 +0000

    [automatic commit] 2025-06-20 10:00:29 UTC 129.127.202.120

--- part1/app.js
+++ part1/app.js
@@ -23,6 +23,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  Error handling logic by 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
