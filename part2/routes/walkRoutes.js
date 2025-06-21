aef5372bb371fe1a741c7f5eb35c33bd8e4004f2
Tang Piseth <tangpiseth43@gmail.com>
2025-06-21 06:52:21 +0000

    [automatic commit] 2025-06-21 06:52:21 UTC ;; communications error to 208.67.222.222#53: timed out 180.150.113.48

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -39,8 +39,6 @@ router.post('/', async (req, res) => {
 // Get walk requests by owner_id
 router.get('/owner/:owner_id', async (req, res) => {
   const ownerID = req.params.owner_id;
-
-
   try{
       const [rows] = await db.query(`
       SELECT wr.*, d.name AS dog_name, d.size, u.username AS owner_name
@@ -52,7 +50,7 @@ router.get('/owner/:owner_id', async (req, res) => {
     `, [ownerID]);
       res.json(rows);
   } catch(error){
-            console.error(`SQL Error (GET /owner/${ownerID}):`, error);
+        console.error(`SQL Error (GET /owner/${ownerID}):`, error);
         res.status(500).json({ error: 'Failed to fetch walks for owner.' });
   }
 });
