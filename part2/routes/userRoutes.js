2da5cf5878c49034715cf6d1c996ea238eb6b3fc
Tang Piseth <tangpiseth43@gmail.com>
2025-06-21 05:04:50 +0000

    [automatic commit] 2025-06-21 05:04:50 UTC 180.150.113.48

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -51,6 +51,9 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
+
+    if (user.password_hash !== password) {
+
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
