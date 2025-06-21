12f478e7c6b9abba294d94e0a0c01510cc81b7eb
Tang Piseth <tangpiseth43@gmail.com>
2025-06-21 04:30:56 +0000

    [automatic commit] 2025-06-21 04:30:56 UTC 180.150.113.48

--- part2/app.js
+++ part2/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Session setup
 app.use(session({
-    secret: 'dog-walking-secret-key', // session secret
+    secret: 'secret-key', // session secret
     resave: false, // don't save session if unmodified
     saveUninitialized: true, // save uninitialized sessions
     cookie: { secure: false } // set to true if using HTTPS
