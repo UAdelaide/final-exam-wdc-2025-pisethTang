3c7dbebed788be7a25b4a748a974baf8e03679e0
Tang Piseth <tangpiseth43@gmail.com>
2025-06-21 06:17:05 +0000

    [automatic commit] 2025-06-21 06:17:05 UTC 180.150.113.48

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -89,7 +89,13 @@ router.post('/login', async (req, res) => {
 // POST logout
 router.post('/logout', (req, res) => {
   req.session.destroy((err) => {
-    if (err)
+    if (err){
+      console.error('Session destruction error:', err);
+      return res.status(500).json({ error: 'Failed to log out' });
+    }
+
+
+    // 
   });
 });
 
